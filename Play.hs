b1b52b8f301643bf865be743a0e6820d11577173
TsunQen <33878645+TsunQen@users.noreply.github.com>
2017-11-21 23:12:47 +0100

    Play file
    testing uploading haskell file

---
+++ Play.hs
@@ -0,0 +1,12 @@
+--doubleme x = x + x
+--doubleUs x y = doubleme x + doubleme y
+--writeText = show "hej"
+--doubleSmallNumber x = if x > 100
+  --                    then x
+    --                  else x*2
+
+areaFyrkant a b = a * b
+areaTriangel b h = (b*h)/2
+areaCirkel r = r*r*pi
+let t = "Längsta ordet är:"
+longestWord a b = if length a > length b then return t ++ a else if length a < length b then return t ++ b else return "Båda är lika långa!"
